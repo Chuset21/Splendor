a542914b59584f30f7fd5bb63f5b01aecbc9ffb7
Miguel Bloggs <arrietam@tcd.ie>
2022-10-26 15:10:13 -0400

    Changed password field to PasswordField type

--- client/src/main/java/hexanome14/LoginScreen.java
+++ client/src/main/java/hexanome14/LoginScreen.java
@@ -2,6 +2,7 @@ package hexanome14;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -14,14 +15,14 @@ public class LoginScreen extends Application {
         Label id = new Label("User ID");
         Label pw = new Label("Password");
         TextField tf1 = new TextField();
-        TextField tf2 = new TextField();
+        PasswordField tf2 = new PasswordField();
         Button b = new Button("Submit");
-        b.setOnAction(e->System.out.println("You entered user ID: " + tf1.getText() + " and password: " + tf2.getText()));
+        b.setOnAction(e -> System.out.println("You entered user ID: " + tf1.getText() + " and password: " + tf2.getText()));
         GridPane root = new GridPane();
         root.addRow(0, id, tf1);
         root.addRow(1, pw, tf2);
         root.addRow(2, b);
-        Scene scene=new Scene(root,800,200);
+        Scene scene = new Scene(root, 800, 200);
         stage.setScene(scene);
         stage.setTitle("Text Field Example");
         stage.show();
