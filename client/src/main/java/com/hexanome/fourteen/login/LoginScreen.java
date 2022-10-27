9e0c368366abee58a0f0451d76bf2195ea71ee90
Miguel Bloggs <arrietam@tcd.ie>
2022-10-26 23:42:54 -0400

    Made splendor text have a grey rectangle around it

--- client/src/main/java/com/hexanome/fourteen/login/LoginScreen.java
+++ client/src/main/java/com/hexanome/fourteen/login/LoginScreen.java
@@ -7,28 +7,37 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
 
 public class LoginScreen extends Application {
+
+    private static final String SPLENDOR_TEXT = "SPLENDOR";
+    private static final String PASSWORD_PROMPT = "Password";
+    private static final String USERNAME_PROMPT = "Username";
+
     @Override
     public void start(Stage stage) {
-        Label id = new Label("User ID");
-        Label pw = new Label("Password");
-        TextField tf = new TextField();
+        Button splendorText = new Button(SPLENDOR_TEXT);
+        splendorText.getStyleClass().add("splendor-title");
+
+        TextField username = new TextField();
+        username.setPromptText(USERNAME_PROMPT);
+
         PasswordField password = new PasswordField();
+        password.setPromptText(PASSWORD_PROMPT);
 
         Button loginButton = createButton(true);
-        loginButton.setOnAction(e -> System.out.printf("You entered user ID: %s and password: %s%n", tf.getText(), password.getText()));
+        loginButton.setOnAction(e -> System.out.printf("You entered user ID: %s and password: %s%n", username.getText(), password.getText()));
 
-        Button quit = createButton(false);
+        Button quitButton = createButton(false);
 
-        GridPane root = new GridPane();
-        root.addRow(0, id, tf);
-        root.addRow(1, pw, password);
-        root.addRow(2, loginButton);
-        root.addRow(0, quit);
+        GridPane grid = new GridPane();
+        grid.add(splendorText, 1, 0);
+        grid.add(quitButton, 2, 0);
+        grid.add(username, 1, 1);
+        grid.add(password, 1, 2);
+        grid.add(loginButton, 1, 3);
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(grid);
         scene.getStylesheets().add("login.css");
 
         stage.setScene(scene);
