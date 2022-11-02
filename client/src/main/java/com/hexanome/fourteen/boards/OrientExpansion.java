b2ce4f3d4301324215c6608c3a43be5d6e5e9a52
kai turanski <kai.turanski@mail.mcgill.ca>
2022-11-02 15:15:04 -0400

    Changed background, fleshed out menu, recolored and organized the reserved and purchased menus.

--- client/src/main/java/com/hexanome/fourteen/boards/OrientExpansion.java
+++ client/src/main/java/com/hexanome/fourteen/boards/OrientExpansion.java
@@ -42,7 +42,7 @@ public class OrientExpansion {
         Parent root = FXMLLoader.load(Objects.requireNonNull(OrientExpansion.class.getResource("OrientExpansionBoard1600x900.fxml")));
 
         // Set up root on stage (window)
-        Scene aScene = new Scene(root);
+        Scene aScene = new Scene(root,1440,810);
 
         // Initialize stage settings
         aPrimaryStage.setScene(aScene);
