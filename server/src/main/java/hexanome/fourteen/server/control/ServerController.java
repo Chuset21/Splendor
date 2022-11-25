c2e18551aae52702eccdeb58c39773b5e8bee4b7
Miguel Bloggs <arrietam@tcd.ie>
2022-11-25 08:59:00 -0500

    Created the user if the first login failed

--- server/src/main/java/hexanome/fourteen/server/control/ServerController.java
+++ server/src/main/java/hexanome/fourteen/server/control/ServerController.java
@@ -38,7 +38,11 @@ public class ServerController {
   @PostConstruct
   private void loginAndRegister() {
     if (!login()) {
-      throw new RuntimeException("Was not able to login");
+      // If login failed, create the user and try again
+      createUser();
+      if (!login()) {
+        throw new RuntimeException("Was not able to login");
+      }
     }
     if (!registerGameService()) {
       throw new RuntimeException("Was not able to register with game service");
@@ -62,8 +66,7 @@ public class ServerController {
     try {
       ResponseEntity<String> responseEntity = rest.exchange(
           "%soauth/token?grant_type=password&username=%s&password=%s".formatted(LS_LOCATION,
-              USERNAME,
-              PASSWORD), HttpMethod.POST, requestEntity, String.class);
+              USERNAME, PASSWORD), HttpMethod.POST, requestEntity, String.class);
       if (responseEntity.getStatusCode().value() != 200) {
         return false;
       }
@@ -76,6 +79,52 @@ public class ServerController {
     } catch (HttpClientErrorException ignored) {
       return false;
     }
+  }
+
+  /**
+   * Create our user.
+   */
+  private void createUser() {
+    RestTemplate rest1 = new RestTemplate();
+    HttpHeaders headers1 = new HttpHeaders();
+    headers1.add("authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
+
+    String body1 =
+        "user_oauth_approval=true&_csrf=19beb2db-3807-4dd5-9f64-6c733462281b&authorize=true";
+
+    HttpEntity<String> requestEntity1 = new HttpEntity<>(body1, headers1);
+    ResponseEntity<String> responseEntity1 = rest1.exchange(
+        "%soauth/token?grant_type=password&username=maex&password=%s".formatted(LS_LOCATION,
+            PASSWORD), HttpMethod.POST, requestEntity1, String.class);
+
+    String curAccessToken =
+        encodePlusSign(gson.fromJson(responseEntity1.getBody(), LoginResponse.class).accessToken());
+
+    // We are logged in as maex
+    RestTemplate rest2 = new RestTemplate();
+    HttpHeaders headers2 = new HttpHeaders();
+    headers2.add("Content-Type", "application/json");
+    headers2.add("authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
+
+    String body2 = """
+        {
+            "name": "%s",
+            "password": "%s",
+            "preferredColour": "01FFFF",
+            "role": "ROLE_SERVICE"
+        }""".formatted(USERNAME, PASSWORD);
+
+    HttpEntity<String> requestEntity2 = new HttpEntity<>(body2, headers2);
+    rest2.exchange(
+        "%sapi/users/%s?access_token=%s".formatted(LS_LOCATION, USERNAME, curAccessToken),
+        HttpMethod.PUT, requestEntity2, String.class);
+
+    // We have now added our user and are going to log out maex
+    RestTemplate rest = new RestTemplate();
+
+    HttpEntity<String> requestEntity = new HttpEntity<>("", new HttpHeaders());
+    rest.exchange("%soauth/active?access_token=%s".formatted(LS_LOCATION, curAccessToken),
+        HttpMethod.DELETE, requestEntity, String.class);
   }
 
   /**
