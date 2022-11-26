e8fb6f435b4498d60f0e12bbb8aa145df91ef7ad
Miguel Bloggs <arrietam@tcd.ie>
2022-11-26 16:03:48 -0500

    Changed how game manager is implemented

--- server/src/main/java/hexanome/fourteen/server/control/LobbyServiceCallbacksController.java
+++ server/src/main/java/hexanome/fourteen/server/control/LobbyServiceCallbacksController.java
@@ -7,6 +7,7 @@ import hexanome.fourteen.server.model.board.Noble;
 import hexanome.fourteen.server.model.board.card.Card;
 import hexanome.fourteen.server.model.board.expansion.Expansion;
 import hexanome.fourteen.server.model.board.player.Player;
+import hexanome.fourteen.server.model.sent.SentGameBoard;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,18 +33,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/games/")
 public class LobbyServiceCallbacksController {
 
-  private final Map<String, GameBoard> gameManager;
+  private final Map<Set<String>, GameBoard> gameManager;
   private final Mapper<User, Player> userPlayerMapper;
   private final Mapper<String, Expansion> stringExpansionMapper;
+  private final Mapper<GameBoard, SentGameBoard> gameBoardMapper;
+  private final GsonInstance gsonInstance;
 
   /**
    * Constructor.
    */
   public LobbyServiceCallbacksController(@Autowired Mapper<User, Player> userPlayerMapper,
                                          @Autowired
-                                         Mapper<String, Expansion> stringExpansionMapper) {
+                                         Mapper<String, Expansion> stringExpansionMapper,
+                                         @Autowired
+                                         Mapper<GameBoard, SentGameBoard> gameBoardMapper,
+                                         @Autowired GsonInstance gsonInstance) {
     this.userPlayerMapper = userPlayerMapper;
     this.stringExpansionMapper = stringExpansionMapper;
+    this.gameBoardMapper = gameBoardMapper;
+    this.gsonInstance = gsonInstance;
     gameManager = new HashMap<>();
   }
 
@@ -54,7 +64,10 @@ public class LobbyServiceCallbacksController {
   @PutMapping(value = "{gameid}", consumes = "application/json; charset=utf-8")
   public ResponseEntity<String> launchGame(@PathVariable String gameid,
                                            @RequestBody LaunchGameForm launchGameForm) {
-    gameManager.put(gameid, createGame(gameid, launchGameForm)); // TODO add checks
+    gameManager.put(
+        Arrays.stream(launchGameForm.players()).map(User::name)
+            .collect(Collectors.toSet()),
+        createGame(gameid, launchGameForm)); // TODO add checks
     return ResponseEntity.status(HttpStatus.OK).body(null);
   }
 
@@ -71,7 +84,51 @@ public class LobbyServiceCallbacksController {
 
   @DeleteMapping("{gameid}")
   public ResponseEntity<String> deleteGame(@PathVariable String gameid) {
-    gameManager.remove(gameid); // TODO implement properly
+    removeGame(gameid); // TODO implement properly
     return ResponseEntity.status(HttpStatus.OK).body(null);
+  }
+
+  /**
+   * Remove a game with the matching game ID.
+   *
+   * @param gameid game ID to remove
+   * @return true if the game was successfully removed, false otherwise.
+   */
+  private boolean removeGame(String gameid) {
+    for (Map.Entry<Set<String>, GameBoard> entry : gameManager.entrySet()) {
+      if (entry.getValue().gameid().equals(gameid)) {
+        gameManager.remove(entry.getKey());
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private GameBoard getGame(String username) {
+    for (Map.Entry<Set<String>, GameBoard> entry : gameManager.entrySet()) {
+      if (entry.getKey().contains(username)) {
+        return entry.getValue();
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Get a game board.
+   *
+   * @param accessToken The user's access token who is sending the request
+   */
+  @GetMapping(produces = "application/json; charset=utf-8")
+  public ResponseEntity<String> retrieveGame(@RequestParam("access_token") String accessToken) {
+    // TODO get username from access token
+    String username = null;
+    final GameBoard gameBoard = getGame(username);
+    if (gameBoard == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game not found.");
+    } else {
+      final SentGameBoard sentGameBoard = gameBoardMapper.map(gameBoard);
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(gsonInstance.gson.toJson(sentGameBoard, SentGameBoard.class));
+    }
   }
 }
