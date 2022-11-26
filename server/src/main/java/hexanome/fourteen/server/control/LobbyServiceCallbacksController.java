7375678bc9873d8b98c461d94b654db30561b222
Miguel Bloggs <arrietam@tcd.ie>
2022-11-25 21:50:09 -0500

    Added a game manager

--- server/src/main/java/hexanome/fourteen/server/control/LobbyServiceCallbacksController.java
+++ server/src/main/java/hexanome/fourteen/server/control/LobbyServiceCallbacksController.java
@@ -1,5 +1,14 @@
 package hexanome.fourteen.server.control;
 
+import hexanome.fourteen.server.Mapper;
+import hexanome.fourteen.server.model.board.GameBoard;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Controller to handle lobby service callbacks.
@@ -16,16 +26,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/games/")
 public class LobbyServiceCallbacksController {
 
+  private Map<String, GameBoard> gameManager;
+  private final Mapper<CreateGameForm, GameBoard> createGameFormGameBoardMapper;
+
+  public LobbyServiceCallbacksController(
+      @Autowired Mapper<CreateGameForm, GameBoard> createGameFormGameBoardMapper) {
+    this.createGameFormGameBoardMapper = createGameFormGameBoardMapper;
+  }
+
+  @PostConstruct
+  private void initGameManager() {
+    gameManager = new HashMap<>();
+  }
+
+  /**
+   * Create a game given the specified parameters.
+   *
+   * @param gameid      The game id for the created game
+   * @param putGameForm The information needed to create the game
+   */
   @PutMapping(value = "{gameid}", consumes = "application/json; charset=utf-8")
   public ResponseEntity<String> launchGame(@PathVariable String gameid,
                                            @RequestBody PutGameForm putGameForm) {
-    new CreateGameForm(putGameForm, gameid); // TODO use
+    gameManager.put(gameid, // TODO add checks
+        createGameFormGameBoardMapper.map(new CreateGameForm(putGameForm, gameid)));
     return ResponseEntity.status(HttpStatus.OK).body(null);
   }
 
   @DeleteMapping("{gameid}")
   public ResponseEntity<String> deleteGame(@PathVariable String gameid) {
-    return ResponseEntity.ok("Game ID: %s received".formatted(gameid)); // TODO implement
+    gameManager.remove(gameid); // TODO implement properly
+    return ResponseEntity.status(HttpStatus.OK).body(null);
   }
-
 }
