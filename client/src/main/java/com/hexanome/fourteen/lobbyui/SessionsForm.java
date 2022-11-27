e755a8daef385b6f8d8ab741f848d3166b7405a3
Miguel Bloggs <arrietam@tcd.ie>
2022-11-27 17:03:44 -0500

    Created sessions form

---
+++ client/src/main/java/com/hexanome/fourteen/lobbyui/SessionsForm.java
@@ -0,0 +1,28 @@
+package com.hexanome.fourteen.lobbyui;
+
+import java.util.Map;
+
+
+/**
+ * Sessions form.
+ */
+public final class SessionsForm {
+  private Map<String, SessionForm> sessions;
+
+  /**
+   * Constructor.
+   */
+  public SessionsForm(Map<String, SessionForm> sessions) {
+    this.sessions = sessions;
+  }
+
+  /**
+   * No args constructor.
+   */
+  public SessionsForm() {
+  }
+
+  public Map<String, SessionForm> sessions() {
+    return sessions;
+  }
+}
