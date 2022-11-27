2ee519ff36365e08d34cc82fa130c4a730dcc4f9
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 16:48:34 -0500

    Added JavaDoc to Mapper.java

--- server/src/main/java/hexanome/fourteen/server/Mapper.java
+++ server/src/main/java/hexanome/fourteen/server/Mapper.java
@@ -7,5 +7,11 @@ package hexanome.fourteen.server;
  * @param <E> Type to map to
  */
 public interface Mapper<T, E> {
+  /**
+   * A Map that allows us to take an Object for Mapping.
+   *
+   * @param t The Object to Map
+   * @return The Mapped Object
+   */
   E map(T t);
 }
