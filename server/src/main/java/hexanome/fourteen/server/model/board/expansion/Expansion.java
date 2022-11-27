155c79070bddb83eecb7141275a32b3cf54da877
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 16:34:16 -0500

    Added JavaDoc to Expansion.java

--- server/src/main/java/hexanome/fourteen/server/model/board/expansion/Expansion.java
+++ server/src/main/java/hexanome/fourteen/server/model/board/expansion/Expansion.java
@@ -4,9 +4,28 @@ package hexanome.fourteen.server.model.board.expansion;
  * Expansion.
  */
 public enum Expansion {
+  /**
+   * Part of the Basic Game.
+   */
   STANDARD,
+
+  /**
+   * Part of the Orient Expansion.
+   */
   ORIENT,
+
+  /**
+   * Part of the Cities Expansion.
+   */
   CITIES,
+
+  /**
+   * Part of the Strongholds Expansion.
+   */
   STRONGHOLDS,
+
+  /**
+   * Part of the Trading Posts Expansion.
+   */
   TRADING_POSTS
 }
