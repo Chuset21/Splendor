d2299e89f216d4a6e11628a5e726f978c1bfb124
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 17:37:11 -0500

    Added JavaDoc to LoginForm.java

--- server/src/main/java/hexanome/fourteen/server/control/LoginForm.java
+++ server/src/main/java/hexanome/fourteen/server/control/LoginForm.java
@@ -16,22 +16,47 @@ public final class LoginForm {
   public LoginForm() {
   }
 
+  /**
+   * A Getter for the Access Token.
+   *
+   * @return The Access Token
+   */
   public String accessToken() {
     return accessToken;
   }
 
+  /**
+   * A Getter for the Token Type.
+   *
+   * @return The Token Type
+   */
   public String tokenType() {
     return tokenType;
   }
 
+  /**
+   * A Getter for the Refresh Token.
+   *
+   * @return The Refresh Token
+   */
   public String refreshToken() {
     return refreshToken;
   }
 
+  /**
+   * A Getter for when the Token expires.
+   *
+   * @return The expiry time of Token
+   */
   public int expiresIn() {
     return expiresIn;
   }
 
+  /**
+   * A Getter for the Scope.
+   *
+   * @return The Scope
+   */
   public String scope() {
     return scope;
   }
