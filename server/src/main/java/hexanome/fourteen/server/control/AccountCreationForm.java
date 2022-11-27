3a6781ae267898afd0c8b806b0c2818581f0d2f4
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 16:22:47 -0500

    Added JavaDoc to AccountCreationForm.java

--- server/src/main/java/hexanome/fourteen/server/control/AccountCreationForm.java
+++ server/src/main/java/hexanome/fourteen/server/control/AccountCreationForm.java
@@ -29,18 +29,38 @@ public class AccountCreationForm {
     role = "ROLE_SERVICE";
   }
 
+  /**
+   * A Getter to get the Name.
+   *
+   * @return Name associated with the account
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * A Getter to get the Password.
+   *
+   * @return Password associated with the account
+   */
   public String getPassword() {
     return password;
   }
 
+  /**
+   * A Getter to get the PreferredColour.
+   *
+   * @return PreferredColour associated with the account
+   */
   public String getPreferredColour() {
     return preferredColour;
   }
 
+  /**
+   * A Getter to get the Role.
+   *
+   * @return Role associated with the account
+   */
   public String getRole() {
     return role;
   }
