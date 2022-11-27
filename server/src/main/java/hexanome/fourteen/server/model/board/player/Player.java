a42e021858dfff32d98cd9b65ab4d82d2ca54f86
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 17:41:14 -0500

    Added JavaDoc to Player.java

--- server/src/main/java/hexanome/fourteen/server/model/board/player/Player.java
+++ server/src/main/java/hexanome/fourteen/server/model/board/player/Player.java
@@ -20,14 +20,29 @@ public final class Player {
     this.hand = hand;
   }
 
+  /**
+   * Constructor when given just the ID.
+   *
+   * @param id ID of the Player
+   */
   public Player(String id) {
     this(id, new Hand());
   }
 
+  /**
+   * A Getter for the User ID.
+   *
+   * @return The User ID
+   */
   public String uid() {
     return uid;
   }
 
+  /**
+   * A Getter for the Hand.
+   *
+   * @return The Hand
+   */
   public Hand hand() {
     return hand;
   }
