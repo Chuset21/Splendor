5c977ef65e7a8d5cf47dfb72e4c7dc5a5606b34e
Krishi Madipelly <madipelk@tcd.ie>
2022-11-27 16:30:58 -0500

    Added JavaDoc to CardLevel.java

--- server/src/main/java/hexanome/fourteen/server/model/board/card/CardLevel.java
+++ server/src/main/java/hexanome/fourteen/server/model/board/card/CardLevel.java
@@ -4,7 +4,18 @@ package hexanome.fourteen.server.model.board.card;
  * Card level.
  */
 public enum CardLevel {
+  /**
+   * Lowest Card Level.
+   */
   ONE,
+
+  /**
+   * Middle Card Level.
+   */
   TWO,
+
+  /**
+   * Highest Card Level.
+   */
   THREE
 }
