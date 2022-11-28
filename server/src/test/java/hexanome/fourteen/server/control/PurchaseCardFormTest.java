0819f027ffe3812e086ae0cf886fd68a9bb6a4a1
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 13:15:36 -0500

    Added basic no args constructor test to PurchaseCardFormTest.java

--- server/src/test/java/hexanome/fourteen/server/control/PurchaseCardFormTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/PurchaseCardFormTest.java
@@ -1,20 +1,36 @@
 package hexanome.fourteen.server.control;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
 class PurchaseCardFormTest {
 
-    @Test
-    void card() {
-    }
+  @Test
+  void noArgsConstructor() {
+    PurchaseCardForm expectedCard = new PurchaseCardForm();
 
-    @Test
-    void gemsToPayWith() {
-    }
+    Assertions.assertNull(expectedCard.card());
+    Assertions.assertNull(expectedCard.gemsToPayWith());
+    Assertions.assertNull(expectedCard.substitutedGems());
+  }
 
-    @Test
-    void substitutedGems() {
-    }
+
+  @Test
+  void card() {
+  }
+
+  @Test
+  void gemsToPayWith() {
+  }
+
+  @Test
+  void substitutedGems() {
+  }
 }
