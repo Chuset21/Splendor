ed93b55e59ca734ba29332d6f1344d35e0a06647
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 13:20:14 -0500

    Optimised import for StandardCardTest.java and corrected CheckStyle

--- server/src/test/java/hexanome/fourteen/server/model/board/card/StandardCardTest.java
+++ server/src/test/java/hexanome/fourteen/server/model/board/card/StandardCardTest.java
@@ -4,72 +4,75 @@ import hexanome.fourteen.server.model.board.expansion.Expansion;
 import hexanome.fourteen.server.model.board.gem.GemColor;
 import hexanome.fourteen.server.model.board.gem.Gems;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StandardCardTest {
 
-    // Testing Object
-    StandardCard standardCard;
+  // Testing Object
+  StandardCard standardCard;
 
-    // Test values
-    Gems cost;
-    final int prestigePoints = 3;
-    final CardLevel cardLevel = CardLevel.THREE;
-    final Expansion expansion = Expansion.STANDARD;
-    final int gemDiscount = 1;
-    final GemColor discountColor = GemColor.RED;
+  // Test values
+  Gems cost;
+  final int prestigePoints = 3;
+  final CardLevel cardLevel = CardLevel.THREE;
+  final Expansion expansion = Expansion.STANDARD;
+  final int gemDiscount = 1;
+  final GemColor discountColor = GemColor.RED;
 
-    @BeforeAll
-    public void setUp() {
-        cost = new Gems();
-        cost.put(GemColor.GREEN, 1);
-        cost.put(GemColor.BLUE, 2);
-        cost.put(GemColor.WHITE, 3);
-        this.standardCard = new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
-    }
+  @BeforeAll
+  public void setUp() {
+    cost = new Gems();
+    cost.put(GemColor.GREEN, 1);
+    cost.put(GemColor.BLUE, 2);
+    cost.put(GemColor.WHITE, 3);
+    this.standardCard =
+        new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
+  }
 
-    @Test
-    public void testPrestigePoints() {
-        this.standardCard = new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
-        assertEquals(prestigePoints, this.standardCard.prestigePoints());
-    }
+  @Test
+  public void testPrestigePoints() {
+    this.standardCard =
+        new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
+    assertEquals(prestigePoints, this.standardCard.prestigePoints());
+  }
 
-    @Test
-    public void testCost() {
-        cost = new Gems();
-        cost.put(GemColor.GREEN, 1);
-        cost.put(GemColor.BLUE, 2);
-        cost.put(GemColor.WHITE, 3);
-        this.standardCard = new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
+  @Test
+  public void testCost() {
+    cost = new Gems();
+    cost.put(GemColor.GREEN, 1);
+    cost.put(GemColor.BLUE, 2);
+    cost.put(GemColor.WHITE, 3);
+    this.standardCard =
+        new StandardCard(prestigePoints, cost, cardLevel, expansion, gemDiscount, discountColor);
 
-        Gems equCost = new Gems();
-        equCost.put(GemColor.GREEN, 1);
-        equCost.put(GemColor.BLUE, 2);
-        equCost.put(GemColor.WHITE, 3);
-        assertEquals(cost, standardCard.cost());
-    }
-    @Test
-    public void level() {
-        assertEquals(cardLevel, standardCard.level());
-    }
+    Gems equCost = new Gems();
+    equCost.put(GemColor.GREEN, 1);
+    equCost.put(GemColor.BLUE, 2);
+    equCost.put(GemColor.WHITE, 3);
+    assertEquals(cost, standardCard.cost());
+  }
 
-    @Test
-    public void expansion() {
-        assertEquals(expansion, standardCard.expansion());
-    }
+  @Test
+  public void level() {
+    assertEquals(cardLevel, standardCard.level());
+  }
 
-    @Test
-    public void gemDiscount() {
-        assertEquals(gemDiscount, standardCard.gemDiscount());
-    }
+  @Test
+  public void expansion() {
+    assertEquals(expansion, standardCard.expansion());
+  }
 
-    @Test
-    public void discountColor() {
-        assertEquals(discountColor, standardCard.discountColor());
-    }
+  @Test
+  public void gemDiscount() {
+    assertEquals(gemDiscount, standardCard.gemDiscount());
+  }
+
+  @Test
+  public void discountColor() {
+    assertEquals(discountColor, standardCard.discountColor());
+  }
 
 
 }
