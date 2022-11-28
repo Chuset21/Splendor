b222855fec081bace865158bdae352fdcae9d5c0
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 13:25:51 -0500

    Added no args constructor test to RegisterGameServiceFormTest.java and fixed CheckStyle

--- server/src/test/java/hexanome/fourteen/server/control/RegisterGameServiceFormTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/RegisterGameServiceFormTest.java
@@ -1,32 +1,49 @@
 package hexanome.fourteen.server.control;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
 class RegisterGameServiceFormTest {
 
-    @Test
-    void location() {
-    }
+  @Test
+  void noArgsConstructor() {
+    RegisterGameServiceForm expectedRegisteredGame = new RegisterGameServiceForm();
 
-    @Test
-    void maxSessionPlayers() {
-    }
+    Assertions.assertNull(expectedRegisteredGame.location());
+    Assertions.assertEquals(0, expectedRegisteredGame.maxSessionPlayers());
+    Assertions.assertEquals(0, expectedRegisteredGame.minSessionPlayers());
+    Assertions.assertNull(expectedRegisteredGame.name());
+    Assertions.assertNull(expectedRegisteredGame.displayName());
+    Assertions.assertNull(expectedRegisteredGame.webSupport());
+  }
 
-    @Test
-    void minSessionPlayers() {
-    }
+  void location() {
+  }
 
-    @Test
-    void name() {
-    }
+  @Test
+  void maxSessionPlayers() {
+  }
 
-    @Test
-    void displayName() {
-    }
+  @Test
+  void minSessionPlayers() {
+  }
 
-    @Test
-    void webSupport() {
-    }
+  @Test
+  void name() {
+  }
+
+  @Test
+  void displayName() {
+  }
+
+  @Test
+  void webSupport() {
+  }
 }
