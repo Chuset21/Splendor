6f187171545faa38ea81f0fb6d92c02dcc9e3b0b
Miguel Bloggs <arrietam@tcd.ie>
2022-11-27 19:04:36 -0500

    Implemented joinSession

--- client/src/main/java/com/hexanome/fourteen/LobbyServiceCaller.java
+++ client/src/main/java/com/hexanome/fourteen/LobbyServiceCaller.java
@@ -23,4 +23,18 @@ public class LobbyServiceCaller {
             .header("authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=").toString(),
         SessionsForm.class);
   }
+
+  /**
+   * Join a session.
+   *
+   * @param player    The player requesting to join
+   * @param authtoken The player's token
+   * @param sessionid The session ID
+   * @return true if successful, false otherwise
+   */
+  public static boolean joinSession(String player, String authtoken, String sessionid) {
+    return Unirest.put("%sapi/sessions/%s/players/%s".formatted(Main.lsLocation, sessionid, player))
+               .header("authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=")
+               .queryString("access_token", authtoken).asEmpty().getStatus() == 200;
+  }
 }
