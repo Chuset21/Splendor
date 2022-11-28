b4be02425714800f9d380cb7ea2a48e1d6b78d22
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 12:18:56 -0500

    Removed Orderer and optimised imports

--- server/src/test/java/hexanome/fourteen/server/control/AccountCreationFormTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/AccountCreationFormTest.java
@@ -3,9 +3,6 @@ package hexanome.fourteen.server.control;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
-
 import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
@@ -13,7 +10,6 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import org.junit.jupiter.api.BeforeAll;
 
 @TestInstance(PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountCreationFormTest {
 
   AccountCreationForm dummyAccount;
