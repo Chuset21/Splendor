634dce1700a0bc15f665048c15b87dccef1e1c7a
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 12:32:01 -0500

    Added Test to GsonInstanceTest.java

--- server/src/test/java/hexanome/fourteen/server/control/GsonInstanceTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/GsonInstanceTest.java
@@ -1,7 +1,28 @@
 package hexanome.fourteen.server.control;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
 class GsonInstanceTest {
 
+  GsonInstance gsonInstance;
+
+  @BeforeAll
+  void setUpGsonInstance() {
+    gsonInstance = new GsonInstance();
+  }
+
+  @Test
+  void noArgsConstructor() {
+    GsonInstance expectedGson = new GsonInstance();
+
+    Assertions.assertNull(expectedGson.gson);
+  }
 }
