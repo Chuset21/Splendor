1a687730d95b1f3ad4a036b1222b2f1ad25628a9
Miguel Bloggs <arrietam@tcd.ie>
2022-11-28 11:03:11 -0500

    Changed NobleForm so that gson can use it

--- client/src/main/java/com/hexanome/fourteen/form/server/NobleForm.java
+++ client/src/main/java/com/hexanome/fourteen/form/server/NobleForm.java
@@ -2,9 +2,33 @@ package com.hexanome.fourteen.form.server;
 
 /**
  * Noble form.
- *
- * @param prestigePoints The amount of prestige points associated with the noble
- * @param cost           The amount of gem discounts needed to acquire the noble
  */
-public record NobleForm(int prestigePoints, GemsForm cost) {
+public final class NobleForm {
+  private int prestigePoints;
+  private GemsForm cost;
+
+  /**
+   * Constructor.
+   *
+   * @param prestigePoints The amount of prestige points associated with the noble
+   * @param cost           The amount of gem discounts needed to acquire the noble
+   */
+  public NobleForm(int prestigePoints, GemsForm cost) {
+    this.prestigePoints = prestigePoints;
+    this.cost = cost;
+  }
+
+  /**
+   * No args constructor.
+   */
+  public NobleForm() {
+  }
+
+  public int prestigePoints() {
+    return prestigePoints;
+  }
+
+  public GemsForm cost() {
+    return cost;
+  }
 }
