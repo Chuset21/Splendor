ebf3152c813bfcb3c37f7287e0655de461e34c48
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 15:02:05 -0500

    Made ServerServiceTest and added template

---
+++ server/src/test/java/hexanome/fourteen/server/control/ServerServiceTest.java
@@ -0,0 +1,18 @@
+package hexanome.fourteen.server.control;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
+public class ServerServiceTest {
+
+  @Test
+  public void refreshToken() {
+  }
+}
