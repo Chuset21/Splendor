472a6ba46923c678242d9299c2c2609952a2744b
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 14:50:49 -0500

    Fixed mvn clean test issue and implemented tests for PurchaseCardFormTest.java

--- server/src/test/java/hexanome/fourteen/server/control/PurchaseCardFormTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/PurchaseCardFormTest.java
@@ -1,5 +1,9 @@
 package hexanome.fourteen.server.control;
 
+import hexanome.fourteen.server.model.board.card.Card;
+import hexanome.fourteen.server.model.board.card.CardLevel;
+import hexanome.fourteen.server.model.board.expansion.Expansion;
+import hexanome.fourteen.server.model.board.gem.Gems;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +14,47 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import org.junit.jupiter.api.BeforeAll;
 
 @TestInstance(PER_CLASS)
-class PurchaseCardFormTest {
+public class PurchaseCardFormTest {
+
+  static Card card;
+  static Gems chosenGems;
+  static Gems substitutedGems;
+
+  static PurchaseCardForm purchasedCard;
+
+  @BeforeAll
+  public static void setUp() {
+    card = new Card() {
+      @Override
+      public int prestigePoints() {
+        return super.prestigePoints();
+      }
+
+      @Override
+      public Gems cost() {
+        return super.cost();
+      }
+
+      @Override
+      public CardLevel level() {
+        return super.level();
+      }
+
+      @Override
+      public Expansion expansion() {
+        return super.expansion();
+      }
+    };
+
+    chosenGems = new Gems();
+
+    substitutedGems = new Gems();
+
+    purchasedCard = new PurchaseCardForm(card, chosenGems, substitutedGems);
+  }
 
   @Test
-  void noArgsConstructor() {
+  public void testNoArgsConstructor() {
     PurchaseCardForm expectedCard = new PurchaseCardForm();
 
     Assertions.assertNull(expectedCard.card());
@@ -23,14 +64,17 @@ class PurchaseCardFormTest {
 
 
   @Test
-  void card() {
+  public void testCard() {
+    Assertions.assertEquals(card, purchasedCard.card());
   }
 
   @Test
-  void gemsToPayWith() {
+  public void testGemsToPayWith() {
+    Assertions.assertEquals(chosenGems, purchasedCard.gemsToPayWith());
   }
 
   @Test
-  void substitutedGems() {
+  public void testSubstitutedGems() {
+    Assertions.assertEquals(substitutedGems, purchasedCard.substitutedGems());
   }
 }
