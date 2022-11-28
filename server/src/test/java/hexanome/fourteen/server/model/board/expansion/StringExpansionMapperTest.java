777a33147c5627cef3c809f745f5ab538043b398
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 15:30:12 -0500

    Added StringExpansionMapperTest.java

---
+++ server/src/test/java/hexanome/fourteen/server/model/board/expansion/StringExpansionMapperTest.java
@@ -0,0 +1,12 @@
+package hexanome.fourteen.server.model.board.expansion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringExpansionMapperTest {
+
+  @Test
+  void map() {
+  }
+}
