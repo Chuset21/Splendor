0c15b8dcdbf6facade092fd82771ea337e550a60
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 13:04:49 -0500

    Fixed CheckStyle on LoginFormTest.java

--- server/src/test/java/hexanome/fourteen/server/control/LoginFormTest.java
+++ server/src/test/java/hexanome/fourteen/server/control/LoginFormTest.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @TestInstance(PER_CLASS)
 class LoginFormTest {
 
-    @Test
-    void noArgsConstructor() {
-        LoginForm expectedLoginForm = new LoginForm();
+  @Test
+  void noArgsConstructor() {
+    LoginForm expectedLoginForm = new LoginForm();
 
-        Assertions.assertNull(expectedLoginForm.accessToken());
-        Assertions.assertNull(expectedLoginForm.tokenType());
-        Assertions.assertNull(expectedLoginForm.refreshToken());
-        Assertions.assertEquals(0, expectedLoginForm.expiresIn());
-        Assertions.assertNull(expectedLoginForm.scope());
-    }
+    Assertions.assertNull(expectedLoginForm.accessToken());
+    Assertions.assertNull(expectedLoginForm.tokenType());
+    Assertions.assertNull(expectedLoginForm.refreshToken());
+    Assertions.assertEquals(0, expectedLoginForm.expiresIn());
+    Assertions.assertNull(expectedLoginForm.scope());
+  }
 }
