75a607f970e43cc0f26d575ec29e959ea293dae0
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 16:12:54 -0500

    Added UserTest.java

---
+++ server/src/test/java/hexanome/fourteen/server/model/UserTest.java
@@ -0,0 +1,22 @@
+package hexanome.fourteen.server.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
+public class UserTest {
+
+  @Test
+  public void name() {
+  }
+
+  @Test
+  public void preferredColour() {
+  }
+}
