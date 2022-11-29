838232adf90758ea37a9ccb8d837cbcfac668c0b
Krishi Madipelly <madipelk@tcd.ie>
2022-11-28 19:46:19 -0500

    Added LobbyServiceTest.java with mvn test starter tests

---
+++ server/src/test/java/hexanome/fourteen/server/control/LobbyServiceTest.java
@@ -0,0 +1,45 @@
+package hexanome.fourteen.server.control;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.BeforeAll;
+
+@TestInstance(PER_CLASS)
+public class LobbyServiceTest {
+  @Test
+  public void login() {
+  }
+
+  @Test
+  public void createUser() {
+  }
+
+  @Test
+  public void logout() {
+  }
+
+  @Test
+  public void getGameServices() {
+  }
+
+  @Test
+  public void registerGameService() {
+  }
+
+  @Test
+  public void refreshToken() {
+  }
+
+  @Test
+  public void unregisterGameService() {
+  }
+
+  @Test
+  public void getUsername() {
+  }
+}
