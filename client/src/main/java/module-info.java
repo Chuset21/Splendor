bc34430026a3d803b0ffea38e82c7ad58bf1ad2f
eamonn-lye <eamonn.lye@mail.mcgill.ca>
2022-11-28 22:27:08 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 5acaf9872fdcfa2c0b216726a919141b6e9ed905, reversing
    changes made to 93df653ccfb4218fd2179e3bc938b16c9620aac4.

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -18,6 +18,4 @@ module com.hexanome.fourteen {
   exports com.hexanome.fourteen.login;
   exports com.hexanome.fourteen.lobbyui;
   exports com.hexanome.fourteen.form.lobbyservice;
-  exports com.hexanome.fourteen.form.server;
-  exports com.hexanome.fourteen.form.server.cardform;
 }
