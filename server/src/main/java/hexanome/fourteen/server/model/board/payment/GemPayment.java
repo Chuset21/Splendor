ca95e37149531ca1abd9db3f09d92dceb8e5e824
Matan Atlas <matan.atlas@mail.mcgill.ca>
2023-02-06 15:32:36 -0500

    GemPayment class

---
+++ server/src/main/java/hexanome/fourteen/server/model/board/payment/GemPayment.java
@@ -0,0 +1,55 @@
+package hexanome.fourteen.server.model.board.payment;
+
+import hexanome.fourteen.server.model.board.gem.Gems;
+
+/**
+ * A class to represent payment using Gems.
+ */
+public class GemPayment implements Payment {
+
+  private Gems chosenGems;
+
+  private Gems substitutedGems;
+
+  private int numGoldGemCards;
+
+  /**
+   * Constructor.
+   *
+   * @param chosenGems        chosen Gems to pay with
+   * @param substitutedGems   required gems provided through goldGemCards
+   * @param numGoldGemCards   number of goldGemCards chosen
+   */
+  public GemPayment(Gems chosenGems, Gems substitutedGems, int numGoldGemCards) {
+    this.chosenGems = chosenGems;
+    this.substitutedGems = substitutedGems;
+    this.numGoldGemCards = numGoldGemCards;
+  }
+
+  /**
+   * A getter for the chosen Gems to pay with.
+   *
+   * @return chosen Gems
+   */
+  public Gems getChosenGems() {
+    return chosenGems;
+  }
+
+  /**
+   * A getter for the number of substituted gems.
+   *
+   * @return substituted Gems
+   */
+  public Gems getSubstitutedGems() {
+    return substitutedGems;
+  }
+
+  /**
+   * A getter for the number of goldGemCards used for payment.
+   *
+   * @return number of goldGemCards
+   */
+  public int getNumGoldGemCards() {
+    return numGoldGemCards;
+  }
+}
