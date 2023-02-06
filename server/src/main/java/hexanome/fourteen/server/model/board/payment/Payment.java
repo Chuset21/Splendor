8483e61df8a893aa3808cb0820d6b09d41140385
Matan Atlas <matan.atlas@mail.mcgill.ca>
2023-02-06 15:24:22 -0500

    Payment interface.

---
+++ server/src/main/java/hexanome/fourteen/server/model/board/payment/Payment.java
@@ -0,0 +1,7 @@
+package hexanome.fourteen.server.model.board.payment;
+
+/**
+ * A payment interface to store how a player will pay for a card they wish to purchase.
+ */
+public interface Payment {
+}
