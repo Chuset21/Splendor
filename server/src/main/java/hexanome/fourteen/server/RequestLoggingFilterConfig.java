fe498b324a4ab702f6cda421102a0b85ae7fda9e
Miguel Bloggs <arrietam@tcd.ie>
2023-03-10 17:52:28 -0500

    Added debug profile that logs requests + responses

---
+++ server/src/main/java/hexanome/fourteen/server/RequestLoggingFilterConfig.java
@@ -0,0 +1,28 @@
+package hexanome.fourteen.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+
+/**
+ * Request logging filter config class to configure the logging filter.
+ */
+@Configuration
+public class RequestLoggingFilterConfig {
+
+  /**
+   * Configure the log filter.
+   *
+   * @return The CommonsRequestLoggingFilter
+   */
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeHeaders(false);
+    return filter;
+  }
+}
