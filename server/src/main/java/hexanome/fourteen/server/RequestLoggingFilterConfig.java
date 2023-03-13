9fa61978180ccb8ad91929e56537a7a069bca36a
Miguel Bloggs <arrietam@tcd.ie>
2023-03-12 22:27:17 -0400

    Removed redundant new line

--- server/src/main/java/hexanome/fourteen/server/RequestLoggingFilterConfig.java
+++ server/src/main/java/hexanome/fourteen/server/RequestLoggingFilterConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
-
 /**
  * Request logging filter config class to configure the logging filter.
  */
