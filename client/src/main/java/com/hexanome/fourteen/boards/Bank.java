0e2db19238abeab043c48e8894e52cd7a9429317
Matan Atlas <matan.atlas@mail.mcgill.ca>
2023-03-30 16:39:12 -0400

    attempt to fix bug that allows to take gems if there are 0 gems available

--- client/src/main/java/com/hexanome/fourteen/boards/Bank.java
+++ client/src/main/java/com/hexanome/fourteen/boards/Bank.java
@@ -341,6 +341,7 @@ public class Bank {
       gameBoard.purchaseCard(purchaseCardForm);
     } else {
       takeBankButton.setDisable(true);
+      updateButtonsForBonusGem();
       toggle();
     }
   }
